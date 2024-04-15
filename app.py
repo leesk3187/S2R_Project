41324b156ef3b0c2abb000a39874bf5d48343b59
parkseunghan <01seunghan@gmail.com>
2024-04-15 15:54:38 +0900

    templates - ip-list.html 생성, app.py 코드 추가, db.py 코드 추가2

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import *
+from db import *
+
+
+app = Flask(__name__)
+app.secret_key = 'test'
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        userid = request.form.get('userid')
+        userpw = request.form.get('userpw')
+
+        user_info = (userid, userpw)
+        result = sql_select(select_query, user_info)
+
+        if result:
+            flash("로그인 성공")
+            return redirect(url_for('index'))
+        else:
+            flash("로그인 실패")
+            return redirect(url_for('login'))
+        
+    else:
+        return render_template("login.html")
+    
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        userid = request.form.get('userid')
+        userpw = request.form.get('userpw')
+
+        user_info = (userid, userpw)
+        result = sql_insert(insert_query, user_info)
+
+        if result == True:
+            flash("회원가입 성공!")
+            return redirect(url_for('login'))
+        else:
+            flash("가입 실패")
+            return redirect(url_for('register'))
+
+    return render_template("register.html")
+
+@app.route('/password')
+def password():
+    return render_template("password.html")
+
+@app.route('/map')
+def map():
+    return render_template("layout-static.html")
+
+@app.route('/ip-list') # IP List 페이지
+def ip_list():
+    ips = get_all_ips() # db.py에서 get_all_ips() 함수 가져옴 = ip 데이터
+    print("ips: ")
+    print(ips)
+    
+    return render_template("ip-list.html", ips=ips) # ip-list.html로 데이터 전달
+
+
+
+
+if __name__ == '__main__':
+ 
+    app.run(host='0.0.0.0', debug=True, port=9999)
+    
+    # 박
---
+++ db.py
@@ -0,0 +1,55 @@
+# db.py
+import pymysql
+
+def get_db_connection():
+    conn = pymysql.connect(host='127.0.0.1', user='root', db='s2r', charset='utf8')
+    return conn
+
+def test_db_connection():
+    conn = pymysql.connect(host='localhost', user='s2r',password='s2r', db='s2r', charset='utf8')
+    return conn
+
+
+def sql_select(query, data):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(query, data)
+        result = cur.fetchall()
+        cur.close()
+        conn.close()
+        return result
+    except pymysql.MySQLError as e:
+        print(f"SQL Error: {e}")
+        return False
+
+def sql_insert(query, data):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(query, data)
+        conn.commit()
+        cur.close()
+        conn.close()
+        return True
+    except pymysql.MySQLError as e:
+        print(f"SQL Error: {e}")
+        return False
+
+def get_all_ips(): # ip 가져오기
+    try:
+        conn = test_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT ip_address FROM ip_table")  # ip주소 컬럼 이름, ip테이블 이름 수정
+        result = cur.fetchall()
+        cur.close()
+        conn.close()
+        return result
+    except pymysql.MySQLError as e:
+        print(f"SQL Error: {e}")
+        return []
+
+
+
+insert_query = "insert into register (userid, userpw) values(%s, %s)"
+select_query = "select userid, userpw from register where userid=%s and userpw=%s"
